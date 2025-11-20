3afa5c1ac9bc0417c8a0262fe8e74dccd4c9dcc1
Facundo  Bravo Scrollini <121453608+facuscrollini@users.noreply.github.com>
2025-11-20 13:00:54 +0000

    Card section en home lista

--- src/front/components/home-components/features-section-components/FeatureCard.jsx
+++ src/front/components/home-components/features-section-components/FeatureCard.jsx
@@ -2,14 +2,18 @@ const FeatureCard = ({ title, description, buttonText,imgUrl, last }) => {
     return (
         <div className={`col-lg-4 ${last ? "col-md-12" : "col-md-6 "} col-12`}>
             <div className="d-flex justify-content-center h-100">
-                <div className="card h-100 rounded-5 overflow-hidden border-0 text-center " style={{width: "15rem"}}>
-                    <img src={imgUrl} className="card-img-top" alt="..."/>
-                        <div className="card-body pb-0 d-flex flex-column align-items-center">
-                            <h5 className="card-title fs-3">{title}</h5>
-                            <p className="card-text">{description}</p>
-                            <div className="pb-5  border-bottom border-color-5">
+                <div className="card h-100 rounded-5 overflow-hidden border-0 text-center back-color-4 p-4 " style={{width: "18rem"}}>
+                    <div className="ratio ratio-1x1">
+                    <img src={imgUrl} className="object-fit-cover rounded-circle" alt="..."/>
+                    </div>
+                        <div className="card-body p-0 d-flex flex-column justify-content-between align-items-center ">
+                            <div className="py-4">
+                            <h5 className="card-title fs-4">{title}</h5>
+                            <p className="card-text ">{description}</p>
+                            </div>
+                            <div className=" border-bottom border-color-5">
 
-                            <a href="#" className="btn button-color-4 font-color-3 rounded-pill ">{buttonText}</a>
+                            <a href="#" className="btn button-color-7 font-color-3 rounded-2 font-color-5 fs-4">{buttonText}</a>
                             </div>
                         </div>
                 </div>
